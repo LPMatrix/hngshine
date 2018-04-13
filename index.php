fcdb2bcf5db4d6eec9f916970f447aa25b086018
Sanusi Mubaraq <mubaraqsanusi908@gmail.com>
2018-04-13 17:52:14 +0100

    added index file

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Shine</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/fontello.css">
+
+</head>
+
+<body>
+    <div class="header-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-2 col-sm-12 col-xs-12">
+                    <div class="logo"><a href="index.html" style="font-weight: bold;">SHINE</a></div>
+                </div>
+                <div class="col-lg-9 col-md-10 col-sm-12 col-xs-12">
+                    <div class="navigation">
+                        <div id="navigation">
+                            <ul>
+                                <li class="active"><a href="index.html" title="Home">Home</a></li>
+                                <li><a href="about.html" title="About us">About us</a> </li>
+                                <li><a href="#" title="Service">Service</a></li>
+                                <li><a href="pricing.html" title="Pricing">Pricing</a></li>
+                                <li><a href="contact-us.html" title="Contact Us">Contact Us</a> </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="space-medium bg-default">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="section-title">
+                    	<h1><?= date("h:i:sa"); ?></h1>
+                        <!-- section title start-->
+                        <h1 class="text-white">Pricing Plan </h1>
+                    </div>
+                    <!-- /.section title start-->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                    <div class="price-head">
+                        <h3> House Cleaning</h3>
+                    </div>
+                    <div class="price-block">
+                        <div class="price-content">
+                            <h4>Kitchen &amp; Bedrooms</h4>
+                            <div class="price-text">
+                                <p>$28/<small style="font-size: 10px;"> hour</small></p>
+                            </div>
+                            <ul>
+                                <li>Wipe Cabinets</li>
+                                <li>Wipes Tops</li>
+                                <li>Dust Tops of Cabinets</li>
+                            </ul>
+                        </div>
+                        <div class="price-content">
+                            <h4>Bathroom</h4>
+                            <div class="price-text">
+                                <p>$29/ <small style="font-size: 10px;">hour</small></p>
+                            </div>
+                            <ul>
+                                <li>Dust Counters</li>
+                                <li>Dust Light</li>
+                                <li>Wipe Countertops</li>
+                            </ul>
+                        </div>
+                        <div class="price-content">
+                            <h4>Living Room</h4>
+                            <div class="price-text">
+                                <p>$29/ <small style="font-size: 10px;">hour</small></p>
+                            </div>
+                            <ul>
+                                <li>Make Beds</li>
+                                <li>Dust Furniture</li>
+                                <li>Dust Mini-blinds</li>
+                            </ul>
+                        </div>
+                        <div class="text-center mt30"><a href="#" class="btn btn-primary btn-sm">book your order now</a></div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                    <div class="price-head">
+                        <h3>Office Cleaning</h3>
+                    </div>
+                    <div class="price-block">
+                        <div class="price-content">
+                            <h4>Once a Week</h4>
+                            <div class="price-text">
+                                <p>$270/<small style="font-size: 10px;"> hour</small></p>
+                            </div>
+                            <ul>
+                                <li>( 4 Sessions - 3 hrs per Session )</li>
+                                
+                            </ul>
+                        </div>
+                        <div class="price-content">
+                            <h4>Twice a Week</h4>
+                            <div class="price-text">
+                                <p>$574/ <small style="font-size: 10px;">hour</small></p>
+                            </div>
+                            <ul>
+                                <li>( 8 Sessions - 3 hrs per Session )</li>
+                                </ul>
+                        </div>
+                        <div class="price-content">
+                            <h4>Thrice a Week</h4>
+                            <div class="price-text">
+                                <p>$670/ <small style="font-size: 10px;">hour</small></p>
+                            </div>
+                            <ul>
+                                <li>( 12 Sessions - 3 hrs per Session )</li>
+                                
+                            </ul>
+                        </div>
+                        <div class="price-content">
+                            <h4>Mon - Fri Daily</h4>
+                            <div class="price-text">
+                                <p>$990/ <small style="font-size: 10px;">hour</small></p>
+                            </div>
+                            <ul>
+                                <li>( 20 Sessions - 2 hrs per Session )</li>
+                                
+                            </ul>
+                        </div>
+                        <div class="text-center mt30"><a href="#" class="btn btn-primary btn-sm">book your order now</a></div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+                    <div class="price-head">
+                        <h3>Commercial Cleaning</h3>
+                    </div>
+                    <div class="price-block">
+                        <div class="price-content">
+                            <h4>Kitchen &amp; Bedrooms</h4>
+                            <div class="price-text">
+                                <p>$28/<small style="font-size: 10px;"> hour</small></p>
+                            </div>
+                            <ul>
+                                <li>Wipe Cabinets</li>
+                                <li>Wipes Tops</li>
+                                <li>Dust Tops of Cabinets</li>
+                            </ul>
+                        </div>
+                        <div class="price-content">
+                            <h4>Bathroom</h4>
+                            <div class="price-text">
+                                <p>$29/ <small style="font-size: 10px;">hour</small></p>
+                            </div>
+                            <ul>
+                                <li>Dust Counters</li>
+                                <li>Dust Light</li>
+                                <li>Wipe Countertops</li>
+                            </ul>
+                        </div>
+                        <div class="price-content">
+                            <h4>Living Room</h4>
+                            <div class="price-text">
+                                <p>$29/ <small style="font-size: 10px;">hour</small></p>
+                            </div>
+                            <ul><li>Make Beds</li>
+                                <li>Dust Furniture</li>
+                                <li>Dust Mini-blinds</li>
+                            </ul>
+                        </div>
+                        <div class="text-center mt30"><a href="#" class="btn btn-primary btn-sm">book your order now</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/jquery.min.js" type="text/javascript"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="js/navigation.js" type="text/javascript"></script>
+    <script src="js/menumaker.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/jquery.sticky.js"></script>
+    <script type="text/javascript" src="js/sticky-header.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="js/slider.js"></script>
+    <script type="text/javascript" src="js/testimonial-slider.js"></script>
+</body>
+
+</html>
